844e010615257817c2fa2a35df76821f89c0d7e1
Kyle Bustard <bustardkyle@gmail.com>
2017-10-06 09:20:02 -0500

    add show User action

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -8,4 +8,9 @@ defmodule RumblWeb.UserController do
     users = Repo.all(User)
     render conn, "index.html", users: users
   end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(User, id)
+    render conn, "show.html", user: user
+  end
 end
