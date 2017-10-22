5372e92febca380ae2418ecb3737892e3663f963
Kyle Bustard <bustardkyle@gmail.com>
2017-10-21 20:17:26 -0500

    build authentication layer, registration changeset to handle passwords

--- lib/rumbl/contexts/user.ex
+++ lib/rumbl/contexts/user.ex
@@ -13,9 +13,26 @@ defmodule Rumbl.User do
     timestamps()
   end
 
+  def registration_changeset(%User{} = user, attrs) do
+    user
+    |> changeset(attrs)
+    |> cast(attrs, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
   def changeset(%User{} = user, attrs \\ %{}) do
     user 
     |> cast(attrs, ~w(name username), [])
     |> validate_length(:username, min: 1, max: 20)
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ -> 
+        changeset
+    end
   end
 end
