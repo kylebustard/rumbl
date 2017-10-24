bb5cae332e1d954a1a480601b79397027a816540
Kyle Bustard <bustardkyle@gmail.com>
2017-10-23 20:09:02 -0500

    create functionality to store users in session

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -25,6 +25,7 @@ defmodule RumblWeb.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
